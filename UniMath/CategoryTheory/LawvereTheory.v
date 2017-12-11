e7ee898dab555cf061a2f8bcf13ade3d1845ac1b
Tim J. Baumann <tim@timbaumann.info>
2017-12-11 18:53:36 +0000

    define models of a lawvere theory

--- UniMath/CategoryTheory/LawvereTheory.v
+++ UniMath/CategoryTheory/LawvereTheory.v
@@ -12,6 +12,7 @@ Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.ProductCategory.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
 Require Import UniMath.CategoryTheory.limits.graphs.limits.
+Require Import UniMath.CategoryTheory.categories.category_hset.
 Local Open Scope cat.
 
 Section LawvereTheory.
@@ -36,12 +37,25 @@ Section FinProductPreservation.
 Context {C D : precategory}.
 Variable (F : functor C D).
 
-Definition preserves_fin_products (n : nat) (d : ∏ (i : stn n), C) (p : C)
-  (cc : ∏ (i : stn n), p --> d i) : UU :=
-    isProductCone (stn n) C d p cc ->
-    isProductCone (stn n) D (fun i => F (d i)) (F p) (fun i => functor_on_morphisms F (cc i)).
+Definition preserves_fin_products : UU :=
+    ∏ (n : nat),
+    ∏ (d : ∏ (i : stn n), C),
+    ∏ (p : C),
+    ∏ (cc : ∏ (i : stn n), p --> d i),
+      isProductCone (stn n) C d p cc ->
+      isProductCone (stn n) D (fun i => F (d i)) (F p) (fun i => functor_on_morphisms F (cc i)).
 
 End FinProductPreservation.
+
+Section Models.
+
+Variable (T : LawvereTheory).
+
+Definition isModel (A : functor (pr1 T) HSET) := preserves_fin_products A.
+Definition Model := total2 (fun x => isModel x).
+
+End Models.
+
 
 (* todo: category of Lawvere theories *)
 (* todo: projections *)
