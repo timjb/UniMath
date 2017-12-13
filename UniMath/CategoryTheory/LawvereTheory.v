e5a8951efde4e12acfaa393282665c2f68f3ef13
Brandon <bdoher@uwo.ca>
2017-12-13 11:23:34 +0000

    cleaned up some style issues, modified definition to assume a Lawvere Theory hs homsets

--- UniMath/CategoryTheory/LawvereTheory.v
+++ UniMath/CategoryTheory/LawvereTheory.v
@@ -16,35 +16,36 @@ Require Import UniMath.CategoryTheory.limits.FinOrdProducts.
 Require Import UniMath.CategoryTheory.categories.category_hset.
 Local Open Scope cat.
 
-Section LawvereTheory.
+Section Lawvere_Theory.
 
-Variable C : precategory.
+(* Definition of a Lawvere Theory*)
 
-Definition isNFoldProduct (t p : C) (n : nat) :=
+Variable C : category.
+
+Definition is_N_Fold_Product (t p : C) (n : nat) :=
   total2 (fun projections => isProductCone (stn n) C (fun _ => t) p projections).
 
-Definition isGeneratingObject (t : C) :=
-   ∥ ∏ (c : C), total2 (isNFoldProduct t c) ∥.
+Definition is_Generating_Object (t : C) :=
+   ∥ ∏ (c : C), total2 (is_N_Fold_Product t c) ∥.
 
-Definition isLawvereTheory :=
-  total2 isGeneratingObject × hasFinOrdProducts C.
+Definition is_Lawvere_Theory :=
+  total2 is_Generating_Object × hasFinOrdProducts C.
 
-End LawvereTheory.
+End Lawvere_Theory.
 
-Definition LawvereTheory := total2 isLawvereTheory.
+Definition Lawvere_Theory := total2 is_Lawvere_Theory.
 
-Definition underlying_precat (T : LawvereTheory) : precategory := pr1 T.
+Definition underlying_category (T : Lawvere_Theory) : category := pr1 T.
 
-Search ishinh.
-Definition generating_object (T : LawvereTheory) : underlying_precat T :=
+Definition generating_object (T : Lawvere_Theory) : underlying_category T :=
   pr1 (pr1 (pr2 T)).
 
-Section FinProductPreservation.
+Section Finite_Product_Preservation.
 
 Context {C D : precategory}.
 Variable (F : functor C D).
 
-Definition preserves_fin_products : UU :=
+Definition preserves_finite_products : UU :=
     ∏ (n : nat),
     ∏ (d : stn n -> C),
     ∏ (p : C),
@@ -52,20 +53,20 @@ Definition preserves_fin_products : UU :=
       isProductCone (stn n) C d p cc ->
       isProductCone (stn n) D (fun i => F (d i)) (F p) (fun i => functor_on_morphisms F (cc i)).
 
-Lemma isaprop_preserves_fin_products : isaprop preserves_fin_products.
+Lemma isaprop_preserves_finite_products : isaprop preserves_finite_products.
 Proof.
   repeat (apply impred; intro).
   apply isapropiscontr.
 Defined.
 
-End FinProductPreservation.
+End Finite_Product_Preservation.
 
-Lemma comp_of_FP_functors_is_FP : 
+Lemma comp_of_fin_prod_preserving_functors_is_fin_prod_preserving : 
 ∏ (C D E : precategory) (F1 : functor C D) (F2 : functor D E), 
-(preserves_fin_products F1) -> (preserves_fin_products F2) -> (preserves_fin_products (F1 ∙ F2)).
+(preserves_finite_products F1) -> (preserves_finite_products F2) -> (preserves_finite_products (F1 ∙ F2)).
 Proof.
   intros.
-  unfold preserves_fin_products.
+  unfold preserves_finite_products.
   intros.
   apply X0.
   apply X.
@@ -74,35 +75,35 @@ Defined.
 
 Section Models.
 
-Variable (T : LawvereTheory).
+Variable (T : Lawvere_Theory).
 
-Definition isModel (A : functor (pr1 T) HSET) := preserves_fin_products A.
+Definition isModel (A : functor (pr1 T) HSET) := preserves_finite_products A.
 Definition Model := total2 (fun x => isModel x).
 
 End Models.
 
 Section Morphisms.
 
-Context {T1 T2: LawvereTheory}.
+Context {T1 T2: Lawvere_Theory}.
 
-Definition preserves_generating_object (F : functor (underlying_precat T1) (underlying_precat T2)) :=
+Definition preserves_generating_object (F : functor (underlying_category T1) (underlying_category T2)) :=
   ∥ iso (F (generating_object T1)) (generating_object T2) ∥.
 
-Definition is_Lawvere_map (F : functor (underlying_precat T1) (underlying_precat T2)) :=
-  (preserves_generating_object F) × (preserves_fin_products F).
+Definition is_Lawvere_map (F : functor (underlying_category T1) (underlying_category T2)) :=
+  (preserves_generating_object F) × (preserves_finite_products F).
 
-Lemma isaprop_is_Lawvere_map (F : functor (underlying_precat T1) (underlying_precat T2)) : isaprop (is_Lawvere_map F).
+Lemma isaprop_is_Lawvere_map (F : functor (underlying_category T1) (underlying_category T2)) : isaprop (is_Lawvere_map F).
 Proof.
   apply isapropdirprod.
   + apply isapropishinh.
-  + apply isaprop_preserves_fin_products.
+  + apply isaprop_preserves_finite_products.
 Defined.
 
 Definition Lawvere_maps := total2 is_Lawvere_map.
 
 End Morphisms.
 
-Lemma identity_is_Lawvere_map : ∏ T: LawvereTheory, is_Lawvere_map (functor_identity (underlying_precat T)).
+Lemma identity_is_Lawvere_map : ∏ T: Lawvere_Theory, is_Lawvere_map (functor_identity (underlying_category T)).
 Proof.
   intro T.
   unfold is_Lawvere_map.
@@ -110,22 +111,22 @@ Proof.
     - simpl.
       apply hinhpr.
       apply identity_iso.
-    - unfold preserves_fin_products.
+    - unfold preserves_finite_products.
       simpl.
       tauto.
 Defined.
 
-Definition precat_of_Lawvere_Theories_ob_mor : precategory_ob_mor.
+Definition precategory_of_Lawvere_Theories_ob_mor : precategory_ob_mor.
 Proof.
    unfold precategory_ob_mor.
-   exists LawvereTheory.
+   exists Lawvere_Theory.
    intros.
    exact (@Lawvere_maps X X0).
    Defined.
 
-Definition precat_of_Lawvere_Theories_data : precategory_data.
+Definition precategory_of_Lawvere_Theories_data : precategory_data.
 Proof.
-   exists precat_of_Lawvere_Theories_ob_mor.
+   exists precategory_of_Lawvere_Theories_ob_mor.
    split.
    - intro c.
    destruct c.
@@ -137,7 +138,6 @@ Proof.
      split.
      + unfold preserves_generating_object. simpl.
        assert (preserves_generating_object (pr1 X0) -> preserves_generating_object (pr1 X) -> ishinh_UU (iso ((pr1 X0) ((pr1 X) (generating_object a))) (generating_object c))).
-         Check factor_through_squash.
          apply (@factor_through_squash (iso (pr1 X0 (generating_object b)) (generating_object c)) (preserves_generating_object (pr1 X) -> _)).
            repeat (apply impred; intro).
            exact (pr2 t0).
@@ -147,24 +147,23 @@ Proof.
            exact (pr2 t).
          intro iso2.
          apply hinhpr.
-         SearchAbout iso.
          eapply iso_comp.
-          Check functor_on_iso.
           apply functor_on_iso. exact iso2.
           exact iso1.
        apply X1.
        exact (pr1 (pr2 X0)).
        exact (pr1 (pr2 X)).
-     + apply comp_of_FP_functors_is_FP.
+     + apply comp_of_fin_prod_preserving_functors_is_fin_prod_preserving.
        exact (pr2 (pr2 X)).
        exact (pr2 (pr2 X0)).
 Defined.
 
-Definition precat_of_Lawevere_Theories : precategory.
+Definition precategory_of_Lawvere_Theories : precategory.
 Proof.
-  exists precat_of_Lawvere_Theories_data.
+  exists precategory_of_Lawvere_Theories_data.
   unfold is_precategory.
-  split. split.
+  split.
+  split.
   - simpl.
     intros.
     unfold compose.
@@ -186,10 +185,11 @@ Proof.
   - intros a b c d f g h.
     apply total2_paths_equiv.
     use tpair.
-    -- simpl. Check functor_assoc. symmetry. apply (functor_assoc (underlying_precat a) (underlying_precat b) (underlying_precat c) (underlying_precat d)).
+    -- simpl.
+       symmetry.
+       apply (functor_assoc (underlying_category a) (underlying_category b) (underlying_category c) (underlying_category d)).
     -- apply isaprop_is_Lawvere_map.
 Qed.
 
 (* todo: is Law univalent? *)
-(* todo: projections *)
 (* todo: models *)
